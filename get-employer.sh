48e81a997d01d3040c61f7d7bebd88c6e00f6b36
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2014-02-19 15:58:21 -0600

    Adding more stuff

---
+++ get-employer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. host.include
+
+if [ "$1" == "" ]; then
+    echo "You must supply a employer bloom ID"
+    exit 1
+fi
+
+employer_bloom_id="$1"
+
+path="/employers/${employer_bloom_id}"
+
+# https://github.com/jkbr/httpie
+echo "http GET ${employer_service}${path}"
+time http GET ${employer_service}${path}
+
--- validate-qe.sh
+++ validate-qe.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Member Service
-service=localhost:8092
+. host.include
+
+# QE Validation
 
 if [ ! -f "$1" ]; then
-    echo "You must supply a valida JSON file"
+    echo "You must supply a valid JSON file"
     exit 1
 fi
 
@@ -13,7 +14,7 @@ member_bloom_id=`grep memberBloomId "$1" |sed -e 's/.*: "//' -e 's/",.*//'`
 path="/members/${member_bloom_id}/events/qualifyingEvent"
 
 # https://github.com/jkbr/httpie
-time http PUT ${service}${path} < $1
+time http PUT ${member_service}${path} < $1
 
 # KSS - Hourly: [317] = 692E0C3C119A11E1942912313F047E54
 # KSS - Salaried: [318] = 692E0CB4119A11E1942912313F047E54
