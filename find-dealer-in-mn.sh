a9bcec236ea838a17b7788ed4930dd824740fb01
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-02-09 23:14:15 -0600

    a new approach to support security tokens

--- find-dealer-in-mn.sh
+++ find-dealer-in-mn.sh
@@ -3,4 +3,4 @@
 lat=45.831335
 lon=-95.383322
 
-./find-dealer.sh $lat $lon
+./find dealer $lat $lon
--- test-login.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ENV=dev
-URL="http://authentication-service-${ENV}.connectedfleet.io:8080/userLogin"
-echo '{"clientId":"OEMPortal","username":"bsmith@test.com","password":"password"}' | http POST $URL
